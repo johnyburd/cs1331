838f905efa9cdc580a3c15b46edb235fbef07860
Jonathan Buchanan <johnyburd@gatech.edu>
2017-11-21 18:18:48 -0500

    ply

--- hw5/src/Ply.java
+++ hw5/src/Ply.java
@@ -1,7 +1,33 @@
 public class Ply {
-    Piece piece;
-    Square to;
-    Square from;
-    Optional<String> comment;
+    private Piece piece;
+    private Square from;
+    private Square to;
+    private Optional<String> comment;
+
+    public Ply(Piece p, Square f, Square t, c) {
+        piece = p;
+        from = f;
+        to = t;
+        comment = c;
+    }
+    public Ply(Piece p, Square f, Square t) {
+        piece = p;
+        from = f;
+        to = t;
+        comment = null;
+    }
+    public Piece getPiece() {
+        return piece;
+    }
+    public Square getTo(){
+        return to;
+    }
+    public Square getFrom(){
+        return from;
+    }
+    public Optional<String> getComment() {
+        return comment;
+    }
+
 
 }
